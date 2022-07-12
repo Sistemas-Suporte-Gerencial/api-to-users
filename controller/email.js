47348b327658deea77127a96aedabe2c9e41d414
mathmartins2 <dev.sincroni@gmail.com>
2022-07-12 17:05:59 -0300

    v14

--- controller/email.js
+++ controller/email.js
@@ -1,23 +1,33 @@
 import {createTransport} from 'nodemailer';
+import {google} from 'googleapis';
+
+const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
+oauth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN});
 
 export const sendEmail = async (req, res) =>{
     try {
         const { to, subject, text, html } = req.body;
 
+        const acessToken = await oauth2Client.getAccessToken();
+
         let transporter = createTransport({
-            host: process.env.SMTP_HOST,
+            service: 'gmail',
             auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
+                type: 'OAuth2',
+                user: 'sgedu@suportegerencial.com.br',
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                acessToken: acessToken,
             }
         });
 
         await transporter.sendMail({
-            from: 'SGEDU-SUPORTEGERENCIAL 👻" <suportegerencial@sgedu.com>',
-            to,
-            subject,
-            text,
-            html
+            from: 'SGEDU-SUPORTEGERENCIAL 👻" <sgedu@suportegerencial.com.br>',
+            to: 'guilhermeyla@gmail.com',
+            subject: 'redbull',
+            text: 'redbull',
+            html: '<b>redbull</b>'
         });
 
         return res.status(200).json({
