0e773f4eccc07737bf974f9cefdf09434be6ef01
mathmartins2 <dev.sincroni@gmail.com>
2022-07-12 19:42:09 -0300

    refatorate token

--- middleware/validToken.js
+++ middleware/validToken.js
@@ -1,15 +1,13 @@
 const tokenFree = [];
 
 const validToken = (req, res, next) => {
-    if(!tokenFree.includes(req.path)){
-        if(req.headers.authorization === process.env.TOKEN) {
-            next();
-        } else {
-            return res.status(401).json({
-                error: 'Unauthorized'
-            });
-        }
-    }
+    if (!tokenFree.includes(req.path)) 
+        return;
+
+    if(!req.headers.authorization === process.env.TOKEN) 
+        return res.status(401).json({error: 'Unauthorized'}); 
+
+    next();
 }
 
 export default validToken;
